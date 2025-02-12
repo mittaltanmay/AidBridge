c0289691925e53dc20a1fa03d8882acf7a7c370b
tanmay mittal <139030702+mittaltanmay@users.noreply.github.com>
2025-02-12 19:37:17 +0530

    Register screen

--- Aidbridge2/app/register.tsx
+++ Aidbridge2/app/register.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 
 const Register = () => {
   return (
     <>
     <View className="circle"></View>
-    <View>
-      <Text>Register</Text>
+    <View className='p-10 flex flex-col items-center justify-center gap-5'>
+      <Text className='text-4xl font-bold'>Register</Text>
+      <View className='flex flex-col gap-2'>
+        <Text className='text-xl'>Name</Text>
+        <TextInput className='bg-white border w-[300px] rounded-md'></TextInput>
+        <Text className='text-xl mt-5'>Country</Text>
+        <TextInput className='bg-white border w-[300px] rounded-md'></TextInput>
+        <Text className='text-xl mt-5'>Contact</Text>
+        <TextInput className='bg-white border w-[300px] rounded-md'></TextInput>
+      </View>
     </View>
     </>
   )
