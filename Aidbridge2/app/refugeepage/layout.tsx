f24c1fa0edaba9da7f9420a1705074fdf5c7c2fc
tanmay mittal <139030702+mittaltanmay@users.noreply.github.com>
2025-03-18 22:07:05 +0530

    Layout Setup for refugee frontend

--- Aidbridge2/app/refugeepage/layout.tsx
+++ Aidbridge2/app/refugeepage/layout.tsx
@@ -1,15 +1,36 @@
-import { View, Text ,StyleSheet,Dimensions} from 'react-native'
-import React from 'react'
+import { View, Text ,StyleSheet,Dimensions,Image} from 'react-native'
+import React , {ReactNode} from 'react'
 import { LinearGradient } from 'expo-linear-gradient'
 const { width, height } = Dimensions.get("window");
-
-export default function Layout () 
+interface LayoutProps {
+  children: ReactNode;
+}
+export default function Layout ({children}:LayoutProps) 
 {
+  function header()
+  {
+    return(
+      <View className='flex h-fit px-2 py-4 flex-row items-center gap-2 border-b border-b-slate-300'>
+        <Image className='w-[50px] h-[50px] rounded-full' source={require('../../assets/images/logo7.webp')}/>
+        <Text className='font-outfit-bold text-2xl mt-4'>Welcome,Tanmay</Text>
+      </View>
+    )
+  }
+  function footer()
+  {
+    return(
+      <View className='flex flex-row border-t border-t-slate-300 h-[80px]'>
+
+      </View>
+    )
+  }
   return (
-    <>
-    <View>
-      <Text>Layout</Text>
+    <View className='flex flex-col h-full'>
+      {header()}
+      <View className='flex flex-col mx-auto mt-10 flex-1'>
+        {children}
+      </View>
+      {footer()}
     </View>
-    </>
   )
 }
